e5b8426def0cd807059c3fc139fbefe7fec806de
memnarch <memnarch@ravni.ca>
2020-07-15 17:03:06 +0900

    deploy

---
+++ precache-manifest.ecd3b863c679227df59e049fdda123df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2035e20a9ac6deb0136",
+    "url": "/reimagined-octo-disco/css/app.7dd720a4.css"
+  },
+  {
+    "revision": "b3acdc8c6822019d3d72",
+    "url": "/reimagined-octo-disco/css/chunk-vendors.fcec92c5.css"
+  },
+  {
+    "revision": "c4c534c673dfadda7363bdf709a75e0f",
+    "url": "/reimagined-octo-disco/css/pokeicon.css"
+  },
+  {
+    "revision": "0f8c911dda6aa61678b8f53e00246941",
+    "url": "/reimagined-octo-disco/img/icon32.png"
+  },
+  {
+    "revision": "56752799a654e26afdb0e9ebbc299366",
+    "url": "/reimagined-octo-disco/index.html"
+  },
+  {
+    "revision": "d2035e20a9ac6deb0136",
+    "url": "/reimagined-octo-disco/js/app.9fa49b8a.js"
+  },
+  {
+    "revision": "b3acdc8c6822019d3d72",
+    "url": "/reimagined-octo-disco/js/chunk-vendors.d91f48e0.js"
+  },
+  {
+    "revision": "c900addb295fe414956a3f0ad3da8fb1",
+    "url": "/reimagined-octo-disco/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/reimagined-octo-disco/robots.txt"
+  }
+]);
